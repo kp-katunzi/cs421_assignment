9d0e42458e2a5c8c6d518fa7fab3529109610886
kp-katunzi <paulkatunzi10@gmail.com>
2025-04-30 01:08:01 +0300

    db.php changes

--- v1/config/db.php
+++ v1/config/db.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'host' => 'localhost',
-    'username' => 'user',
-    'password' => 'Aaronpaul@100',
+    'host' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
     'database' => 'api_db',
     'charset' => 'utf8mb4'
 ];
